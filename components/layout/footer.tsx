e0f8ebdec36935c8cc01ae14f11ab40f19871a06
kjxbyz <kjxbyz888@outlook.com>
2023-06-10 14:53:12 +0800

    update footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,16 +1,7 @@
-"use client";
-import useScroll from "@/lib/hooks/use-scroll";
-
 export default function Footer() {
-  const scrolled = useScroll(65);
-
   return (
     <div
-      className={`fixed bottom-0 w-full ${
-        !scrolled
-          ? "border-b border-gray-200 bg-white/50 backdrop-blur-xl"
-          : "bg-white/0"
-      } py-5 text-center`}
+      className="absolute w-full border-b border-gray-200 py-5 text-center"
     >
       <p className="text-gray-500">
         Source code hosted on{" "}
