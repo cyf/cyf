542256a4bcfcc1c3694236a452d96b5e00e10b52
kjxbyz <kjxbyz@163.com>
2023-08-08 06:29:34 +0800

    fix: ts-5.1.6

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default async function RootLayout({
       <body className={cx(sfPro.variable, inter.variable)}>
         <div className="fixed h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100" />
         <Suspense fallback="...">
-          {/* @ts-expect-error Server Component */}
+          {/* @ts-ignore */}
           <Nav />
         </Suspense>
         <main
