cee9cfbd6712049808c84a677e7d09696ce0d641
kjxbyz <kjxbyz@163.com>
2023-08-10 14:52:31 +0800

    add links

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -15,6 +15,25 @@ export default function Footer() {
         </a>
       </p>
       <p className="mt-2 flex items-center justify-center">
+        <a
+          className="font-medium text-gray-800 underline transition-colors"
+          href="https://chenyifaer.com/fafa-runner/privacy"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Privacy Policy
+        </a>
+        &nbsp;&nbsp;
+        <a
+          className="font-medium text-gray-800 underline transition-colors"
+          href="https://chenyifaer.com/fafa-runner/terms-of-use"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Terms &amp; Conditions
+        </a>
+      </p>
+      <p className="mt-2 flex items-center justify-center">
         <Image
           src="https://visitor-badge.laobi.icu/badge?page_id=chenyifaer.com"
           width={60}
