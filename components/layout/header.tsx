b890e870398a72f07bac9fb9fd323178df0267b0
kjxbyz <kjxbyz@163.com>
2023-10-07 22:49:56 +0800

    feat: toggle menu

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { MdLiveTv } from "react-icons/md";
+import { BiTestTube } from "react-icons/bi";
+import { IoGameControllerOutline } from "react-icons/io5";
 import useScroll from "@/lib/hooks/use-scroll";
 import LngDropdown from "./lng-dropdown";
 import ThemeDropdown from "./theme-dropdown";
@@ -14,6 +16,12 @@ export default function Header(props: LngProps) {
   const { t } = useTranslation(props.lng, "header");
   const scrolled = useScroll(50);
   const router = useRouter();
+
+  // toggle menu
+  const toggleMenu = () => {
+    const $navbar = document.querySelector("#navbar-language");
+    $navbar?.classList.toggle("hidden");
+  };
 
   return (
     <div
@@ -37,18 +45,68 @@ export default function Header(props: LngProps) {
           ></Image>
           <p>{t("title")}</p>
         </Link>
-        <div>
-          <div className="relative inline-block text-left">
-            <button
-              onClick={() => router.push(`/${props.lng}/live`)}
-              className="mr-2 flex h-8 w-8 items-center justify-center overflow-hidden rounded-full transition-all duration-75 focus:outline-none active:scale-95 sm:h-9 sm:w-9"
-            >
-              <MdLiveTv className="h-5 w-5" />
-            </button>
-          </div>
-          <LngDropdown lng={props.lng} />
-          <ThemeDropdown lng={props.lng} />
+        <div
+          className="w-18 hidden items-center justify-between max-md:absolute max-md:right-5 max-md:top-16 md:order-1 md:flex md:w-auto"
+          id="navbar-language"
+        >
+          <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 p-4 font-medium dark:border-gray-700 md:mt-0 md:flex-row md:space-x-0 md:border-0 md:p-0">
+            <li>
+              <button
+                onClick={() =>
+                  router.push("https://www.chenyifaer.com/fafa-runner")
+                }
+                className="mx-1 flex h-8 w-8 items-center justify-center overflow-hidden rounded-full transition-all duration-75 focus:outline-none active:scale-95 sm:h-9 sm:w-9"
+              >
+                <IoGameControllerOutline className="h-5 w-5" />
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={() => router.push("https://www.chenyifaer.com/join")}
+                className="mx-1 flex h-8 w-8 items-center justify-center overflow-hidden rounded-full transition-all duration-75 focus:outline-none active:scale-95 sm:h-9 sm:w-9"
+              >
+                <BiTestTube className="h-5 w-5" />
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={() => router.push(`/${props.lng}/live`)}
+                className="mx-1 flex h-8 w-8 items-center justify-center overflow-hidden rounded-full transition-all duration-75 focus:outline-none active:scale-95 sm:h-9 sm:w-9"
+              >
+                <MdLiveTv className="h-5 w-5" />
+              </button>
+            </li>
+            <li>
+              <LngDropdown lng={props.lng} />
+            </li>
+            <li>
+              <ThemeDropdown lng={props.lng} />
+            </li>
+          </ul>
         </div>
+        <button
+          onClick={toggleMenu}
+          className="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
+          aria-controls="navbar-language"
+          aria-expanded="false"
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="h-5 w-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
       </div>
     </div>
   );
