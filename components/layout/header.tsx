0d92bf369a9262e6912d0474e6c288e36bb7d18d
kjxbyz <kjxbyz888@outlook.com>
2023-10-07 20:41:02 +0800

    fix: add live link in header

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -2,6 +2,8 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { MdLiveTv } from "react-icons/md";
 import useScroll from "@/lib/hooks/use-scroll";
 import LngDropdown from "./lng-dropdown";
 import ThemeDropdown from "./theme-dropdown";
@@ -11,6 +13,7 @@ import { useTranslation } from "@/i18n/client";
 export default function Header(props: LngProps) {
   const { t } = useTranslation(props.lng, "header");
   const scrolled = useScroll(50);
+  const router = useRouter();
 
   return (
     <div
@@ -21,7 +24,10 @@ export default function Header(props: LngProps) {
       } z-30 transition-all`}
     >
       <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between xl:mx-auto">
-        <Link href="/" className="flex items-center font-display text-2xl">
+        <Link
+          href={`/${props.lng}`}
+          className="flex items-center font-display text-2xl"
+        >
           <Image
             src="/portal/logo.jpg"
             alt="logo"
@@ -32,6 +38,14 @@ export default function Header(props: LngProps) {
           <p>{t("title")}</p>
         </Link>
         <div>
+          <div className="relative inline-block text-left">
+            <button
+              onClick={() => router.push(`/${props.lng}/live`)}
+              className="mr-2 flex h-8 w-8 items-center justify-center overflow-hidden rounded-full transition-all duration-75 focus:outline-none active:scale-95 sm:h-9 sm:w-9"
+            >
+              <MdLiveTv className="h-5 w-5" />
+            </button>
+          </div>
           <LngDropdown lng={props.lng} />
           <ThemeDropdown lng={props.lng} />
         </div>
