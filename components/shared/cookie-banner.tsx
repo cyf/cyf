b31bd8eba04c7135b05e6de5bd91629a7ad85df5
kjxbyz <kjxbyz888@outlook.com>
2023-11-09 20:22:24 +0800

    feat: add cookie banner

--- components/shared/cookie-banner.tsx
+++ components/shared/cookie-banner.tsx
@@ -10,20 +10,9 @@ const CookieBanner = (props: LngProps) => {
   return (
     <>
       <Script
-        id="Cookiebot"
-        src="https://consent.cookiebot.com/uc.js"
-        data-culture={props.lng.toUpperCase()}
-        data-cbid={NEXT_PUBLIC_COOKIE_BANNER_ID}
-        data-blockingmode="auto"
-        strategy="afterInteractive"
+        id="cookieyes"
+        src={`https://cdn-cookieyes.com/client_data/${NEXT_PUBLIC_COOKIE_BANNER_ID}/script.js`}
       />
-      {/*<Script*/}
-      {/*  id="CookieDeclaration"*/}
-      {/*  src={`https://consent.cookiebot.com/${NEXT_PUBLIC_COOKIE_BANNER_ID}/cd.js`}*/}
-      {/*  data-culture={props.lng.toUpperCase()}*/}
-      {/*  strategy="afterInteractive"*/}
-      {/*  async*/}
-      {/*/>*/}
     </>
   );
 };
