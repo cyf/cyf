245fbd67165dcf136ee678d6d5e7898638681b01
kjxbyz <kjxbyz888@outlook.com>
2024-02-29 21:05:01 +0800

    fix: 支持全站置灰

--- docs/app/[lng]/layout.tsx
+++ docs/app/[lng]/layout.tsx
@@ -17,7 +17,11 @@ import ReduxProvider from "./redux-provider";
 import ThemeProvider from "./theme-provider";
 import Particles from "./particles";
 
+// 是否显示背景特效
 const NEXT_PUBLIC_SHOW_PARTICLES = process.env.NEXT_PUBLIC_SHOW_PARTICLES;
+// 是否全站置灰
+const NEXT_PUBLIC_WEBSITE_GLOBAL_GRAY =
+  process.env.NEXT_PUBLIC_WEBSITE_GLOBAL_GRAY;
 
 const Header = dynamic(() => import("@/components/layout/header"), {
   ssr: false,
@@ -61,7 +65,12 @@ export default async function RootLayout({
   };
 }) {
   return (
-    <html lang={params.lng} dir={dir(params.lng)} suppressHydrationWarning>
+    <html
+      lang={params.lng}
+      dir={dir(params.lng)}
+      className={NEXT_PUBLIC_WEBSITE_GLOBAL_GRAY ? "grayscale" : ""}
+      suppressHydrationWarning
+    >
       <body className={cx(sfPro.variable, inter.variable)}>
         <NextTopLoader height={1} />
         <ReduxProvider>
--- docs/process.d.ts
+++ docs/process.d.ts
@@ -5,6 +5,7 @@ declare namespace NodeJS {
     NEXT_PUBLIC_GOOGLE_ID: string;
     NEXT_PUBLIC_COOKIE_BANNER_ID: string;
     NEXT_PUBLIC_SHOW_PARTICLES: string;
+    NEXT_PUBLIC_WEBSITE_GLOBAL_GRAY: string;
     API_BASE_URL: string;
     ENCRYPT_KEY: string;
     ENCRYPT_IV: string;
