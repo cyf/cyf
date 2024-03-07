40a8512eba276edbeb809ef043154d03d0d2970d
kjxbyz <kjxbyz@163.com>
2024-03-07 12:12:38 +0800

    fix: update metadata

--- docs/app/[lng]/layout.tsx
+++ docs/app/[lng]/layout.tsx
@@ -32,6 +32,11 @@ export function generateViewport(): Viewport {
   return {
     colorScheme: "dark",
     themeColor: "black",
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+    minimumScale: 1,
+    userScalable: false,
   };
 }
 
