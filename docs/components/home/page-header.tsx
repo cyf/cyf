d74dc2647893922b62ed5669290a63e9fc8ae424
kjxbyz <kjxbyz888@outlook.com>
2024-03-11 23:27:17 +0800

    feat: add form page

--- docs/components/home/page-header.tsx
+++ docs/components/home/page-header.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { basePath } from "@/constants";
-import { useTranslation } from "@/i18n/client";
-import type { LngProps } from "@/i18next-lng";
 
-export default function PageHeader({ lng }: LngProps) {
-  const { t } = useTranslation(lng, "login");
-
+export default function PageHeader({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) {
   return (
     <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center dark:border-gray-700 dark:bg-gray-900 sm:px-16">
       <Link href="/">
@@ -19,8 +21,8 @@ export default function PageHeader({ lng }: LngProps) {
           height={20}
         />
       </Link>
-      <h3 className="text-xl font-semibold">{t("title")}</h3>
-      <p className="text-sm text-gray-500">{t("tips")}</p>
+      <h3 className="text-xl font-semibold">{title}</h3>
+      <p className="text-sm text-gray-500">{description}</p>
     </div>
   );
 }
