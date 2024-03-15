561d3a9934521393416f4f22fd166d4c4984381a
kjxbyz <kjxbyz888@outlook.com>
2024-03-16 00:53:40 +0800

    feat: 内测计划编辑功能

--- docs/services/insider.ts
+++ docs/services/insider.ts
@@ -11,3 +11,16 @@ export const create = (data: Record<string, any>): Promise<Result<Insider>> => {
 export const list = (): Promise<Result<Insider[]>> => {
   return axios.get("/api/insider");
 };
+
+// 获取内测计划详情
+export const findOne = (id: string): Promise<Result<Insider>> => {
+  return axios.get(`/api/insider/${id}`);
+};
+
+// 更新内测计划详情
+export const update = (
+  id: string,
+  data: Record<string, any>,
+): Promise<Result<Insider>> => {
+  return axios.patch(`/api/insider/${id}`, data);
+};
